7481abf6597c79d0466bf1085b8e1bd5683a88b8
zeina1i <hosseinzeinalii@gmail.com>
2025-01-31 17:29:31 +0100

    Fix

--- src/main.rs
+++ src/main.rs
@@ -243,7 +243,7 @@ fn main() {
                                 original.clone()
                             }
                         };
-                        format!("Number {} {}", s, formatted)
+                        format!("NUMBER {} {}", s, formatted)
                     },
                     Token::Equal => "EQUAL = null".to_string(),
                     Token::EqualEqual => "EQUAL_EQUAL == null".to_string(),
